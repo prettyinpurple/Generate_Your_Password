3b6f888a8b28262c7673167bf5ae771a31012b34
MJB <cw1855@hotmail.com>
2023-02-13 17:04:35 -0500

    password characteristics

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length = prompt("How many characters would you like the password to contain");
-if (length < 8) {
-  alert("please enter a number no less than 8 and not greater than 128");
-} else {}
+
+
 
 
 // Write password to the #password input
@@ -12,6 +10,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+  passwordText.length = 128;
+  passwordText.special = " '!,@#$%^&*()?+-./\=;.:><{}[]_`~|";
+  passwordText.numeric = "0,1,2,3,4,5,6,7,8,9";
+  passwordText.lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,u,s,t,u,v,w,x,y,z";
+  passwordText.uppercase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+  
 
 }
 
@@ -19,3 +24,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+confirm("Click OK to confirm using special characters.\nEither OK or Cancel.")
+confirm("Click OK to confirm using numeric charcters. \nEither OK or Cancel.")
+confirm("Click OK to confirm using lowercase characters. \nEither OK or Cancel.")
+confirm("Click OK to confirm using upper case characters.\nEither OK or Cancel.")
+
+
+passwordText.length = prompt("How many characters would you like the password to contain");
+if (passwordText.length < 8, passwordText.length > 128) {
+  alert("Please enter a number no less than 8 and no greater than 128");
+};
+
+
