5454f6c3b24a2eccadd7d666c22389eb1262ae34
MJB <cw1855@hotmail.com>
2023-02-12 19:03:52 -0500

    added prompt

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = prompt("How many characters would you like the password to contain");
+if (length < 8) {
+  alert("please enter a number no less than 8 and not greater than 128");
+} else {}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +14,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
